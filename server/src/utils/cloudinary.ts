9684c8c8df26b233e08127d87e90755d9fcb3e1a
Saurav Pant <sauravpant777@gmail.com>
2025-07-19 19:34:18 +0545

    Add utility functions for API responses, error handling, async handling, and Cloudinary integration

---
+++ server/src/utils/cloudinary.ts
@@ -0,0 +1,33 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME!,
+  api_key: process.env.CLOUDINARY_API_KEY!,
+  api_secret: process.env.CLOUDINARY_SECRET_KEY!,
+});
+
+export const uploadToCloudinary = async (filePath: string): Promise<any> => {
+  try {
+    if (!filePath) return null;
+    const response = await cloudinary.uploader.upload(filePath, {
+      resource_type: "image",
+      folder:"BhetBato"
+    });
+    fs.unlinkSync(filePath);
+    return response;
+  } catch (err) {
+    fs.existsSync(filePath) && fs.unlinkSync(filePath);
+    throw err;
+  }
+};
+
+export const deleteFromCloudinary = async (publicId: string): Promise<void> => {
+  try {
+    await cloudinary.uploader.destroy(publicId, {
+      resource_type: "auto",
+    });
+  } catch (err) {
+    throw err;
+  }
+};
