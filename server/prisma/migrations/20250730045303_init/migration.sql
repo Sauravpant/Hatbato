c734df912976b8628c401fa8c8e0fe897bffadcb
Saurav Pant <sauravpant777@gmail.com>
2025-07-30 11:40:03 +0545

    Add Otp model and migration for OTP functionality

---
+++ server/prisma/migrations/20250730045303_init/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "Otp" (
+    "email" TEXT NOT NULL,
+    "otp" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Otp_pkey" PRIMARY KEY ("email")
+);
