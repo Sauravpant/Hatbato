8b7bbffa050692a5d116f40837c9daad3bcf470a
Saurav Pant <sauravpant777@gmail.com>
2025-09-09 01:52:48 +0545

    feat: Implement user authentication and context for search functionality

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,43 @@
-import React from "react";
+import { useEffect } from "react";
+import { api } from "./lib/axios";
+import { setUser } from "./features/auth/authSlice";
+import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import type { AppDispatch } from "./store/store";
+import { Toaster } from "react-hot-toast";
+import AppRoutes from "./routes/AppRoutes";
+import SearchContextProvider from "./store/searchContext";
 
 const App = () => {
-  return <div>App</div>;
+  const dispatch = useDispatch<AppDispatch>();
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const getUser = async (): Promise<void> => {
+      try {
+        //Hit API endpoint to fetch the user using the access token from the cookies
+        const user = await api.get("/user/me");
+        dispatch(setUser(user.data.data));
+      } catch (err: any) {
+        //Hit API endpoint to get the new access token if the refresh token exists
+        try {
+          const user = await api.get("/auth/refresh-token");
+          dispatch(setUser(user.data.data));
+        } catch (error) {
+          navigate("/");
+        }
+      }
+    };
+    getUser();
+  }, []);
+
+  return (
+    <SearchContextProvider>
+      <AppRoutes />
+      <Toaster />
+    </SearchContextProvider>
+  );
 };
 
 export default App;
