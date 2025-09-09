affcee653b6c8ac69316a52956c94640d756981d
Saurav Pant <sauravpant777@gmail.com>
2025-09-10 00:09:57 +0545

    feat: Add Spinner component

---
+++ client/src/components/ui/Spinner.tsx
@@ -0,0 +1,6 @@
+
+const Spinner = () => {
+  return <div className="inline-block animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-blue-500 mb-4"></div>;
+};
+
+export default Spinner;
