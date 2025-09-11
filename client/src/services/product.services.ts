d5a9569cb36979420ff800fbc0512682320a7e5c
Saurav Pant <sauravpant777@gmail.com>
2025-09-11 14:43:12 +0545

    feat: add getProduct function to retrieve product details by ID

--- client/src/services/product.services.ts
+++ client/src/services/product.services.ts
@@ -1,4 +1,5 @@
 import { api } from "@/lib/axios";
+import type { Product } from "@/types/product/types";
 
 export const postProduct = async (data: FormData) => {
   const response = await api.post("/user/product/create", data);
@@ -7,5 +8,10 @@ export const postProduct = async (data: FormData) => {
 
 export const getAllProducts = async (params?: Record<string, any>) => {
   const response = await api.get("/user/product", { params });
+  return response.data.data;
+};
+
+export const getProductById = async (id: string): Promise<Product> => {
+  const response = await api.get(`/user/product/get/${id}`);
   return response.data.data;
 };
