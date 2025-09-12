9a1572fdf00bcd7a2c614c34c7a03dc44f2f103d
Saurav Pant <sauravpant777@gmail.com>
2025-09-12 14:17:26 +0545

    feat: enhance DialogBox component with submission handling and input fields

--- client/src/components/ui/DialogBox.tsx
+++ client/src/components/ui/DialogBox.tsx
@@ -10,14 +10,25 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
+import { Label } from "./label";
+import { Input } from "./input";
+import { useState } from "react";
 
 interface DialogBoxProps {
   buttonText: string;
   title: string;
   description: string;
+  isSubmitting: boolean;
+  onSubmit: (data: { reason: string; description: string }) => void;
 }
 
-export const DialogBox: React.FC<DialogBoxProps> = ({ title, buttonText, description }) => {
+export const DialogBox: React.FC<DialogBoxProps> = ({ title, buttonText, description, isSubmitting, onSubmit }) => {
+  const [reason, setReason] = useState<string>("");
+  const [details, setDetails] = useState<string>("");
+
+  const handleSubmit = () => {
+    onSubmit({ reason, description: details });
+  };
   return (
     <Dialog>
       <DialogTrigger asChild>
@@ -30,10 +41,20 @@ export const DialogBox: React.FC<DialogBoxProps> = ({ title, buttonText, descrip
           <DialogTitle>{title}</DialogTitle>
           <DialogDescription>{description}</DialogDescription>
         </DialogHeader>
+        <div className="grid gap-4">
+          <div className="grid gap-3">
+            <Label htmlFor="reason">Reason</Label>
+            <Input id="reason" type="text" onChange={(e) => setReason(e.target.value)} className=" outline-none focus:ring-2 focus:ring-blue-500" />
+          </div>
+          <div className="grid gap-3">
+            <Label htmlFor="details">Description</Label>
+            <Input id="details" type="text" onChange={(e) => setDetails(e.target.value)} />
+          </div>
+        </div>
         <DialogFooter className="sm:justify-start">
           <DialogClose asChild>
-            <Button type="button" variant={buttonText !== "Buy Now" ? "destructive" : "secondary"} className="cursor-pointer">
-              {buttonText}
+            <Button type="button" variant={"destructive"} className="cursor-pointer" onClick={handleSubmit}>
+              {isSubmitting ? "Submitting" : buttonText}
             </Button>
           </DialogClose>
         </DialogFooter>
