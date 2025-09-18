9600d371c1d526dfde5e3001bc7b90ae222a1085
Saurav Pant <sauravpant777@gmail.com>
2025-09-18 20:53:46 +0545

    fix: Update axios base URL

--- client/src/lib/axios.ts
+++ client/src/lib/axios.ts
@@ -1,6 +1,9 @@
 import axios from "axios";
 
+const environment =import.meta.env.VITE_ENV
+const BASE_URL=import.meta.env.VITE_BASE_URL
+
 export const api = axios.create({
-  baseURL: "http://localhost:3000/api/v1",
+  baseURL: environment ==="production"?`${BASE_URL}/api/v1`:"http://localhost:3000/api/v1",
   withCredentials: true,
 });
